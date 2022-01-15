2a1a3e0a90bf36e54968aeb11fca721f139de7da
rajoy99 <rafat.joy99@gmail.com>
2022-01-15 23:38:19 +0600

    capital letter test

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react'
 
 
-const Hello = (props) => {
+
+const Hello = () =>{
+
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old. Your score is {props.score}
-      </p>
+      Hello World
+    </div>
+
+  )
+
+
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
     </div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
 
+const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} score='68'/>
-      <Hello name={name} age={age} score='88'/>
+      <Hello name="Maya" age={26 + 10} />
+      <Footer />
     </div>
   )
 }
