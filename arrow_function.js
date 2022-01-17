cab9bc70d208548ee7c019c992c00d76e64c88c0
rajoy99 <rafat.joy99@gmail.com>
2022-01-17 17:34:58 +0600

    arrow function testing

---
+++ arrow_function.js
@@ -0,0 +1,35 @@
+const adder = (a,b)=> {
+
+    return a+b;
+
+}
+
+function adder2(a,b){
+    return a+b;
+}
+
+const adder3 = function(a,b) {
+    return a+b;
+}
+
+
+/// one argument 
+
+const skuare = (p) => {
+    return p*p;
+}
+
+const skuare2= p =>{
+    return p*p
+}
+
+const skuare3=p=>p*p 
+
+
+console.log(adder(11,19))
+console.log(adder2(11,19))
+console.log(adder3(11,19))
+
+console.log(skuare(3))
+console.log(skuare2(3))
+console.log(skuare3(3))
