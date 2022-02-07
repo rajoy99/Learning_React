dbb46adacd50a8738cbb7ddfd5f3a9baf136d26d
rajoy99 <rafat.joy99@gmail.com>
2022-02-07 19:06:55 +0600

    creating of my own

---
+++ currency_my/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CurrencyInput from './CurrencyInput'
+
+function App() {
+  return (
+    <div className="App">
+      <CurrencyInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ currency_my/src/CurrencyInput.js
@@ -0,0 +1,15 @@
+import React  from "react"
+
+const CurrencyInput = () => {
+
+    return(
+        <div className="group">
+            <input type='number' value={621} />
+            <select></select>
+
+        </div>
+    )
+
+}
+
+export default CurrencyInput
